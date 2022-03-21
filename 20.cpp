4799977ab702a12aa75cde5a04194060074cbf2b
Piyush Lohiya <lohiya.piyush04@gmail.com>
2022-03-21 23:23:01 +0530

    12) basic stack

---
+++ 20.cpp
@@ -0,0 +1,43 @@
+
+// basic stack question
+
+class Solution
+{
+public:
+    vector<int> findErrorNums(vector<int> &nums)
+    {
+
+        int cnt = 1;
+        int n = nums.size();
+        int x, y;
+        //         map<int,int> m;
+        //         for(int i=0;i<n;++i)
+        //         {
+        //           ++m[nums[i]];
+        //             if(m[nums[i]]==2)
+        //                 x=nums[i];
+
+        //         }
+
+        //         for(int i=1;i<=n;++i)
+        //         {
+        //             if(m[i]==0)
+        //                 y=i;
+        //         }
+
+        for (int i = 0; i < n; ++i)
+        {
+            if (nums[abs(nums[i]) - 1] < 0)
+                x = abs(nums[i]);
+            else
+                nums[abs(nums[i]) - 1] = -nums[abs(nums[i]) - 1];
+        }
+        for (int i = 0; i < n; ++i)
+        {
+            if (nums[i] > 0)
+                y = i + 1;
+        }
+
+        return {x, y};
+    }
+};
